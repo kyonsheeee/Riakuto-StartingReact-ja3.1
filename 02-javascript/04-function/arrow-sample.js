02fccb181cf0c668a5fa2d3c6d2008f5e2ed7a43
Kyoko Akatsuka <kyokoakatsuka38@gmail.com>
2022-05-13 17:29:51 +0900

    add comment

--- 02-javascript/04-function/arrow-sample.js
+++ 02-javascript/04-function/arrow-sample.js
@@ -2,10 +2,12 @@ const plusOne = function (n) {
   return n + 1;
 }
 
+// アロー関数
 const addOne = (n) => {
   return n + 1;
 };
 
+// アロー関数の省略記法
 const increment = n => n + 1;
 
 console.log(plusOne(1));
