5e3519407245a12934e9d2a0f06abf317a8afadb
Kyoko Akatsuka <kyokoakatsuka38@gmail.com>
2022-05-16 13:49:44 +0900

    5/16進捗

--- 02-javascript/06-object-syntax/destructure.js
+++ 02-javascript/06-object-syntax/destructure.js
@@ -4,3 +4,5 @@ console.log(n, m);
 const obj = { name: 'Kanae', age: 24 };
 const { name, age } = obj;
 console.log(name, age);
+
+// const name = obj.name;  だと、分割代入を使えと怒られる
--- 02-javascript/06-object-syntax/spread.js
+++ 02-javascript/06-object-syntax/spread.js
@@ -5,3 +5,5 @@ console.log(arr2);
 const obj1 = { a: 1, b: 2, c: 3, d: 4 };
 const obj2 = { ...obj1, d: 99, e: 5 };
 console.log(obj2);
+
+// ...の書き方をスプレッド構文という
