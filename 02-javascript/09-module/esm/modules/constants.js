be555a8ee2a161673a964dbb0d1161fd4be033a5
Kyoko Akatsuka <kyokoakatsuka38@gmail.com>
2022-05-19 11:16:46 +0900

    5/19進捗

--- 02-javascript/09-module/esm/modules/constants.js
+++ 02-javascript/09-module/esm/modules/constants.js
@@ -1,5 +1,5 @@
 const ONE = 1;
 const TWO = 2;
 
-export { ONE, TWO };
+export { ONE, TWO };  // 名前ありのエクスポート
 export const TEN = 10;
--- 02-javascript/09-module/esm/modules/math.js
+++ 02-javascript/09-module/esm/modules/math.js
@@ -3,4 +3,5 @@ import { ONE, TWO as ZWEI } from './constants.js';
 export const plus = (n, m = ONE) => n + m;
 const times = (n, m = ZWEI) => n * m;
 
-export default times;
+export default times;  // 名前なしのエクスポート
+// デフォルトエクスポートは1モジュール(1ファイル)につき1回
