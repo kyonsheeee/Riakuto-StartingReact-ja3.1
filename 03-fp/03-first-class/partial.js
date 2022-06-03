429e6f871d1d3afed16b1f7f82bcdc0a806248c4
Kyoko Akatsuka <kyokoakatsuka38@gmail.com>
2022-06-03 12:39:48 +0900

    6/3進捗

--- 03-fp/03-first-class/partial.js
+++ 03-fp/03-first-class/partial.js
@@ -1,5 +1,5 @@
 const withMultiple = (n) => (m) => n * m;
-console.log(withMultiple(3)(5));
+console.log(withMultiple(3)(5));  // 15
 
 const triple = withMultiple(3);
-console.log(triple(5));
+console.log(triple(5));  // 15
