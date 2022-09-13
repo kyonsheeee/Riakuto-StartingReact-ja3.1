1553d41b29db2af9dc98e8183f694c258d67d322
Kyoko Akatsuka <kyokoakatsuka38@gmail.com>
2022-09-13 16:34:42 +0900

    モジュール

---
+++ 04-typescript/07-module/dts/main.ts
@@ -0,0 +1,7 @@
+import { transform, Brooch, CosmicCompact, CrisisCompact } from "./sailormoon-transform";
+
+transform();
+transform(new Brooch());
+transform(new CosmicCompact());
+transform(new CrisisCompact());
+// transform(10);  /* compile error */
